3ccc21c1af968510142c9923b1e074485b8e3e2b
Chetan Gupta <76566118+Chetan2708@users.noreply.github.com>
2023-12-24 23:59:06 +0530

    Update Login.jsx

--- vite-project/src/components/Login.jsx
+++ vite-project/src/components/Login.jsx
@@ -4,6 +4,7 @@ import Group from '../assets/Group 3.png';
 import { useNavigate } from 'react-router-dom';
 const Login = () => {
   const [email, setEmail] = useState('');
+
   const [password, setPassword] = useState('');
   const navigate = useNavigate();
 
